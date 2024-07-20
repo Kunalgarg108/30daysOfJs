617905591ff13920a23a3a8e26ded9f3d27c2543
KUNAL KUMAR  GARG <123102108@nitkkr.ac.in>
2024-07-20 19:01:58 +0530

    added day2.js file

---
+++ day2.js
@@ -0,0 +1,71 @@
+// Task 1
+let m=21;
+let n=13;
+console.log(m+n); // 34
+
+// Task 2
+let p=25;
+let q=10;
+console.log(p-q); // 15
+
+//Task 3
+let a=5;
+let b=8;
+console.log(a*b); // 40
+
+//Task 4
+let c=60;
+let d=20;
+console.log(c/d); // 3
+
+//Task 5
+let i=49;
+let j=7;
+console.log(i%j); // 0
+
+//Task 6
+let k=5;
+k+=2;
+console.log(k); // 7
+
+//Task 7
+let l=5;
+l-=1;
+console.log(l); // 4
+
+
+
+//Task 8
+console.log(9>3); // true
+console.log(9<5); // false
+
+//Task 9
+console.log(9>=9); // true
+console.log(4<=3); // false
+
+//Task 10
+let t=25;
+let r="25";
+console.log(t==r);  // true
+console.log(t===r); // false
+
+//Task 11
+let v=64;
+while(v>0 && v%2==0){
+    v=v/2;}
+console.log(v); // 1
+
+//Task 12
+let age=21;
+if(age==18 || age>18){
+    console.log("eligible for Driving")}
+else{
+    console.log("Not eligible");} //eligible for Driving
+
+//Task 13
+if(!false){
+    console.log("executed");} //executed
+
+//Task 14
+let num=-19;
+console.log(num>=0 ? "positive" : "negative"); //Output:Negative
